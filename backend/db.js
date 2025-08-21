da2f5e9cdb6a2b18ef1fc3b4eb24be94ff0913ea
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2025-08-21 04:10:19 +0000

    Removed previously created contents by jules

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const knex = require('knex');
-const knexfile = require('../knexfile');
-
-// In development, we use the 'development' configuration
-// In production, you might want to use a different configuration
-const db = knex(knexfile.development);
-
-module.exports = db;
